e879fbaeb718facf52bfc774ccbbc07f5d1f45b0
AppreciativeGeek <mnpencildoodle@gmail.com>
2022-12-05 17:40:48 -0800

    refactor settign up db

--- Neptune/script.js
+++ Neptune/script.js
@@ -1,21 +1,7 @@
 
-//CONNECTING TO DB, change links under firebaseConfig to connect to db
-const firebaseConfig = {
-  apiKey: "AIzaSyAO1aIe_fTZB6duj8YIRyYcLTINlcP196w",
-  authDomain: "escouting-7b4e0.firebaseapp.com",
-  databaseURL: "https://escouting-7b4e0-default-rtdb.firebaseio.com",
-  projectId: "escouting-7b4e0",
-  storageBucket: "escouting-7b4e0.appspot.com",
-  messagingSenderId: "377179821867",
-  appId: "1:377179821867:web:cedab35ab708c12986976e",
-  measurementId: "G-8VWYRF9QY6"
-};
-
-const app = initializeApp(firebaseConfig);
-const database = to(app);
+const dataStructure = new DataStructure();
+const db = dataStructure.getFireBase();
 console.log("success")
-
-const db = to();
 
 //
 //=======================
